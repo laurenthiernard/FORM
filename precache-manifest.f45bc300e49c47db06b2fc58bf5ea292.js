6b17492344a1b8561848e7557b59d5933d34eb74
laurent.hiernard <laurent.hiernard@xam-consulting.com>
2020-10-27 23:27:53 +1100

    first commit

---
+++ precache-manifest.f45bc300e49c47db06b2fc58bf5ea292.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c0a877e2a9325215de3ef49c4dab0e3",
+    "url": "/FORM/index.html"
+  },
+  {
+    "revision": "42172d28b7a1caea46ac",
+    "url": "/FORM/static/css/main.384e7616.chunk.css"
+  },
+  {
+    "revision": "6fba37a1985b3fafbe83",
+    "url": "/FORM/static/js/2.298421fb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/FORM/static/js/2.298421fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42172d28b7a1caea46ac",
+    "url": "/FORM/static/js/main.368571b6.chunk.js"
+  },
+  {
+    "revision": "98016019deee2103da19",
+    "url": "/FORM/static/js/runtime-main.3f87af13.js"
+  }
+]);
